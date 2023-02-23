2c48a8a2e19bba7aa7c6b34ea13204870a1c73ea
Shubham Patel <Shubham.Patel@einfochips.com>
2023-02-23 12:22:57 +0530

    Selenium Training - Day1

---
+++ demo1_basics/demo1.py
@@ -0,0 +1,21 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.implicitly_wait(30)
+driver.get("https://www.facebook.com/")
+
+# driver.find_element(By.ID, "email").send_keys("shubham@gmail.com")
+# driver.find_element(By.ID, "pass").send_keys("shubham")
+# driver.find_element(By.NAME, "login").click()
+driver.find_element(By.LINK_TEXT, "Create new account").click()
+driver.find_element(By.NAME, "firstname").send_keys("John")
+driver.find_element(By.NAME, "lastname").send_keys("dina")
+driver.find_element(By.ID, "password_step_input").send_keys("welcome123")
+driver.find_element(By.XPATH, "//input[@value='-1']").click()
+driver.find_element(By.NAME, "websubmit").click()
+time.sleep(5)
+driver.quit()
