dc5b05756cc23e7030b8c9006f243bb4a75c1c9f
Shubham Patel <Shubham.Patel@einfochips.com>
2023-02-24 11:14:30 +0530

    Salesforce automation

--- demo1_basics/demo2_register.py
+++ demo1_basics/demo2_register.py
@@ -1,3 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import time
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.implicitly_wait(30)
+driver.get("https://www.facebook.com/")
 
+
+driver.find_element(By.LINK_TEXT, "Create new account").click()
+driver.find_element(By.NAME, "firstname").send_keys("John")
+driver.find_element(By.NAME, "lastname").send_keys("dina")
+driver.find_element(By.ID, "password_step_input").send_keys("welcome123")
+driver.find_element(By.XPATH, "//input[@value='-1']").click()
+driver.find_element(By.NAME, "websubmit").click()
+time.sleep(5)
+driver.quit()
---
+++ demo1_basics/demo3_salesforce.py
@@ -0,0 +1,24 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.implicitly_wait(10)
+
+driver.get("https://www.salesforce.com/in/form/signup/freetrial-sales/")
+
+driver.find_element(By.NAME, "UserFirstName").send_keys("Shubham")
+driver.find_element(By.NAME, "UserLastName").send_keys("Patel")
+driver.find_element(By.NAME,"UserEmail").send_keys("Shubham@gmail.com")
+Select(driver.find_element(By.NAME,"UserTitle")).select_by_value("IT_Manager_AP")
+driver.find_element(By.NAME,"CompanyName").send_keys("SalesForce")
+Select(driver.find_element(By.NAME,"CompanyEmployees")).select_by_value("1600")
+driver.find_element(By.NAME,"UserPhone").send_keys("1234567891")
+Select(driver.find_element(By.NAME,"CompanyCountry")).select_by_visible_text("India")
+driver.find_element(By.CLASS_NAME,"checkbox-ui").click()
+driver.find_element(By.NAME,"start my free trial").click()
+
+time.sleep(10)
